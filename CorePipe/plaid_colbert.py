29c8fd2b1bab8c5c7dc9215b12eb64c3c2f9ad40
adolf <adolf1321794021@gmail.com>
2024-03-15 18:19:06 +0800

    update

--- CorePipe/plaid_colbert.py
+++ CorePipe/plaid_colbert.py
@@ -5,12 +5,21 @@
 # @Email        : adolf1321794021@gmail.com
 # @LastEditTime : 2024/3/13 17:01
 # @File         : plaid_colbert.py
-# import os
+# from RagPro.stores import PLAIDDocumentStore
+#
+# store = PLAIDDocumentStore(index_path="index/test/",
+#                            checkpoint_path="model/ColBERT-NQ",
+#                            collection_path="demo_data/collection.tsv",
+#                            create=True)
 
-from RagPro.stores import PLAIDDocumentStore
+from colbert import Indexer
+from colbert.infra import Run, RunConfig, ColBERTConfig
 
-# os.environ['KMP_DUPLICATE_LIB_OK'] = True
-
-store = PLAIDDocumentStore(index_path="index/",
-                           checkpoint_path="model/ColBERT-NQ",
-                           collection_path="demo_data/collection.tsv")
+if __name__ == '__main__':
+    with Run().context(RunConfig(nranks=1, experiment="msmarco")):
+        config = ColBERTConfig(
+            nbits=2,
+            root="index/test/",
+        )
+        indexer = Indexer(checkpoint="model/ColBERT-NQ", config=config)
+        indexer.index(name="msmarco.nbits=2", collection="demo_data/collection.tsv")
